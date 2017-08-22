4b28dfd1a59af87a1d7a44d85857c94eabc693c5
aravindg506 <31232161+aravindg506@users.noreply.github.com>
2017-08-22 01:03:45 -0400

    commit

---
+++ Sample2/Controllers/DeptController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sample2.Models;
+
+namespace Sample2.Controllers
+{
+    public class DeptController : Controller
+    {
+        //
+        // GET: /Dept/
+
+        public ActionResult Index()
+        {
+            TESTEntities model = new TESTEntities();
+            List<tblDepartment> department= model.tblDepartments.ToList();
+            return View(department);
+        }
+       
+
+    }
+}
---
+++ Sample2/Controllers/EmpController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sample2.Models;
+
+namespace Sample2.Controllers
+{
+    public class EmpController : Controller
+    {
+        //
+        // GET: /Emp/
+
+        public ActionResult Index(int id)
+        {
+            TESTEntities model = new TESTEntities();
+            List<tblEmployee> emplist = model.tblEmployees.Where(a => a.DepartmentId == id).ToList();
+            return View(emplist);
+        }
+
+        public ActionResult Index1(int id)
+        {
+            TESTEntities model = new TESTEntities();
+            tblEmployee emplist = model.tblEmployees.Where(a => a.EmployeeId == id).Single();
+            return View(emplist);
+        }
+
+    }
+}
